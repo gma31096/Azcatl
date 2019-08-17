3f448f763c5992037b1caf48237b0835d2190949
RogerM <rogemerino.229@gmail.com>
2019-08-17 13:02:13 -0500

    Se comenta el código y se depura

--- src/simple_move/src/simple_move_node.cpp
+++ src/simple_move/src/simple_move_node.cpp
@@ -8,43 +8,25 @@ tf::StampedTransform transform;
 tf::Quaternion q;
 tf::TransformListener *transformListener;
 
+// Función que llama al servicio
 bool simpleMoveCallback(simple_move::SimpleMove::Request &req , simple_move::SimpleMove::Response &resp){
-	//Estados estado = me_inicio;
  	ros::Rate rate(30);
-
-	//float velocidadCrucero = 0;
-	//int i = 0;
-	//bool withDistance;
-	
-	//float goalx,goaly,goalt;
-	//float robotx,roboty,robott;
-
 	bool listo = false;
-	//float error;
-
-	//geometry_msgs::Twist twist;
-	//geometry_msgs::Twist zero_twist;
-	//zero_twist.linear.x = 0;
-	//zero_twist.linear.y = 0;
-	//zero_twist.angular.z = 0;
-
 	while(ros::ok() && !listo){
 		printf("se solicito:\n distancia: %f\n  angulo: %f\n",req.distance,req.angle);	
 		listo = true;	
 	}
-	printf("temine el servicio\n");
+	printf("Temine el servicio\n");
 }
 
 int main(int argc,char ** argv){
-	
 	ros::init(argc,argv,"simple_move_node");
 	ros::NodeHandle nh;
 	ros::Rate rate(30);
-
+	// Se instancía e servicio "simpleMoveService" del nodehandle que a su vez invoca la función simpleMoveCallback cuando se detona el servicio 
 	ros::ServiceServer simpleMoveService = nh.advertiseService("simple_move",simpleMoveCallback);
 
 	while(ros::ok){
-		
 		ros::spinOnce();
 		rate.sleep();
 	}
